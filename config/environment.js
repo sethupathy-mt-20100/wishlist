58393d47376de9ce257f29c75ae05258f5afee07
sethupathy.mt <sethupathy.mt@zohocorp.com>
2025-11-08 19:23:46 +0530

    repo changes

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'wish-list',
     environment,
-    rootURL: '/',
+    rootURL: '/app/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
